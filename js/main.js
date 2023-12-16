a68e81fa12ef82cd76608cec23f2a1274638de71
Marcus Chan <marcuschanjh@gmail.com>
2023-12-17 01:24:40 +0800

    Update Navigation to be seperated out a bit Add in cv winBox logic

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ const about = document.querySelector('#about');
 const contact = document.querySelector('#contact');
 const aboutContent = document.querySelector('#about-content');
 const contactContent = document.querySelector('#contact-content');
+const cv = document.querySelector('#cv');
+const cvContent = document.querySelector('#cv-content');
 
-function openWinBox(title, mountContent, top, right, bottom, left) {
+
+function openWinBox(title, mountContent,width = '400px',height='400px', top = 'center', right = 'center', bottom = 'center', left = 'center') {
     return new WinBox({
         title: title,
-        width: '400px',
-        height: '400px',
+        width: width,
+        height: height,
         top: top,
         right: right,
         bottom: bottom,
@@ -23,9 +26,27 @@ function openWinBox(title, mountContent, top, right, bottom, left) {
 }
 
 about.addEventListener('click', () => {
-    openWinBox('About Me', aboutContent, 50, 50, 50, 50);
+    openWinBox('About Me', aboutContent);
 });
 
 contact.addEventListener('click', () => {
-    openWinBox('Contact Me', contactContent, 150, 50, 50, 250);
+    openWinBox('Contact Me', contactContent);
+});
+
+cv.addEventListener('click', () => {
+    openWinBox('Curriculum Vitae', cvContent,'800px','1000px');
+
+    // Replace 'YOUR_GOOGLE_DRIVE_FILE_ID' with the actual ID of your PDF file on Google Drive
+    const pdfDriveFileId = '123';
+
+    // Create an <iframe> tag for the PDF hosted on Google Drive
+    const pdfIframe = document.createElement('iframe');
+    pdfIframe.src = `https://drive.google.com/file/d/${pdfDriveFileId}/preview`;
+    pdfIframe.style.width = '100%';
+    pdfIframe.style.height = '90%';
+    pdfIframe.style.border = 'none';
+
+    // Replace the existing content in the cv-pdf-container with the PDF viewer
+    document.getElementById('cv-pdf-container').innerHTML = '';
+    document.getElementById('cv-pdf-container').appendChild(pdfIframe);
 });
